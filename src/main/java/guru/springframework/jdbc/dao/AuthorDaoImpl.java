d1ebe7147ce29189300faa1ca17d6b9d6b38a2d9
ddmitry <dvdmit@gmail.com>
2023-10-29 13:20:59 +0300

    Module 9 Assignment

--- src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
+++ src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
@@ -4,18 +4,101 @@ import guru.springframework.jdbc.domain.Author;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
+import java.sql.*;
 
 @Component
 public class AuthorDaoImpl implements AuthorDao {
 
-    DataSource dataSource;
+    private final DataSource source;
 
-    public AuthorDaoImpl(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public AuthorDaoImpl(DataSource source) {
+        this.source = source;
     }
 
     @Override
     public Author getById(Long id) {
+        Connection connection = null;
+        PreparedStatement ps = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = source.getConnection();
+            ps = connection.prepareStatement("SELECT * FROM author where id = ?");
+            ps.setLong(1, id);
+            resultSet = ps.executeQuery();
+
+            if (resultSet.next()) {
+                Author author = new Author();
+                author.setId(id);
+                author.setFirstName(resultSet.getString("first_name"));
+                author.setLastName(resultSet.getString("last_name"));
+
+                return author;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+
+                if (ps != null){
+                    ps.close();
+                }
+
+                if (connection != null){
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public Author findAuthorByName(String firstName, String lastName) {
+        Connection connection = null;
+        PreparedStatement ps = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = source.getConnection();
+            ps = connection.prepareStatement("SELECT * FROM author where first_name = ? and last_name = ?");
+            ps.setString(1, firstName);
+            ps.setString(2, lastName);
+            resultSet = ps.executeQuery();
+
+            if (resultSet.next()) {
+                Author author = new Author();
+                author.setId(resultSet.getLong("id"));
+                author.setFirstName(resultSet.getString("first_name"));
+                author.setLastName(resultSet.getString("last_name"));
+
+                return author;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+
+                if (ps != null){
+                    ps.close();
+                }
+
+                if (connection != null){
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
         return null;
     }
 }
